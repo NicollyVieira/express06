1355f3251c566193775d842ecbbef931790095ac
cauan <cauangerlach297@gmail.com>
2024-04-15 16:11:44 -0300

    isso ai  brow

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const mysql_config = require('./mysql_config');
+
+const functions = require('./functions');
+
+app.listen(3000,()=>{
+    console.log("servidor no ar nessa tarde chuvosa")
+})
+
+app.use(cors());
+
+const connection = mysql.createConnection(mysql_config);
+
+
+app.get('/',(req,res)=>{
+    connection.query('SELECT * FROM tasks',(err,results)=>{
+        if(err){
+            res.json(functions.response('casa caiu',"Erro:"+err.message))
+        }else{
+            res.json(functions.response('Tamu faceiro',"tasks listadas com sucesso",results))
+        }
+    })
+})
+
